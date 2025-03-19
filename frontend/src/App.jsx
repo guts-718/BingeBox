0c583319ea6ce010e85ae5be40d77140cab75d2d
guts-718 <rahul718rg@gmail.com>
2025-03-19 08:17:54 +0530

    BingeBox completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ import { Loader } from "lucide-react";
 import { useAuthStore } from "./store/authUser.js";
 import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/404";
+import WatchPage from "./pages/WatchPage";
+
 function App(){
   const {user,isCheckingAuth,authCheck}=useAuthStore();
   useEffect(()=>{
@@ -27,6 +32,11 @@ function App(){
  <Route path='/' element={<HomePage />} />
         <Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
         <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
+        <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+                <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+                <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+                <Route path='/*' element={<NotFoundPage />} />
+
     </Routes>
      <Footer/>
      <Toaster/>
